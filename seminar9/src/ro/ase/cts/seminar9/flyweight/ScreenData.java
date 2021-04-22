465c4d37502841bd91c4fc8f6cd4745e0a2f4eba
Dumitrascu Sandra-Maria-Monica <dumitrascusandra18@stud.ase.ro>
2021-04-22 09:21:41 +0300

    seminar9 final

---
+++ seminar9/src/ro/ase/cts/seminar9/flyweight/ScreenData.java
@@ -0,0 +1,29 @@
+package ro.ase.cts.seminar9.flyweight;
+
+public class ScreenData { //stare temporara
+
+	private String color;
+	private double x,y,z;
+	
+	public ScreenData(String color, double x, double y, double z) {
+		super();
+		this.color = color;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	public String getColor() {
+		return color;
+	}
+	public double getX() {
+		return x;
+	}
+	public double getY() {
+		return y;
+	}
+	public double getZ() {
+		return z;
+	}
+	
+	
+}
