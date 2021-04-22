bf2c96f37c464cbe263c55e735a043d6abe88b51
Dumitrascu Sandra-Maria-Monica <dumitrascusandra18@stud.ase.ro>
2021-04-22 08:22:28 +0300

    seminar9 break

---
+++ seminar9/src/ro/ase/cts/seminar9/proxy/YoutubeLibraryInterface.java
@@ -0,0 +1,8 @@
+package ro.ase.cts.seminar9.proxy;
+
+public interface YoutubeLibraryInterface {
+	
+	public String listVideos();//afisam id-ul si denumirea
+	public String getVideoInfo(int id);
+
+}
